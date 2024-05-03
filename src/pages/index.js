f36e0f853c213dab6ed05229f53175643a9693c8
jacksonluciano <jackson.ides@gmail.com>
2024-05-03 13:01:18 -0300

    Page Lisboa

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ const HomeOne = () => {
   return (
     <MenuContextProvider>
       <SearchContextProvider>
-        <Layout PageTitle="Home">
+        <Layout PageTitle="Home Lisboa">
           <HeaderOne />
           <HeroVideo />
           <OEvex />
